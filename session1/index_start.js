65334770d9f30d5581cb001461a70f1a675be5a3
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-02-20 09:21:11 -0800

    fizzbuzz recompleted

--- session1/index_start.js
+++ session1/index_start.js
@@ -78,7 +78,7 @@ function maxCharacter(str) {
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 function fizzBuzz() {
   for (let i = 1; i <= 100; i++) {
-    if (i % 3 & (i % 5 === 0)) {
+    if (i % 15 === 0) {
       console.log("FizzBuzz");
     } else if (i % 3 === 0) {
       console.log("Fizz");
@@ -91,6 +91,6 @@ function fizzBuzz() {
 }
 
 // Call Function
-const output = maxCharacter("javascript");
+const output = fizzBuzz();
 
 console.log(output);
