e92c3027cb8b97c2a0e104415b5e506ac83e6e9b
Mateen Kazia <mateencode@gmail.com>
2019-04-04 13:13:43 -0700

    seak and destory completed

--- session3/index_start.js
+++ session3/index_start.js
@@ -35,8 +35,8 @@ function sumAllPrimes(num) {
 // Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-function seekAndDestroy() {
-  let result = [];
+function seekAndDestroy(arr, ...rest) {
+  return arr.filter(val => !rest.includes(val));
 }
 
 // CHALLENGE 4: SORT BY HEIGHT
