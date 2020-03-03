47bf2833a17287778c19b30beb2d78d28502dc68
Mateen Kazia <mateencode@gmail.com>
2020-03-03 16:28:40 -0600

    session 1 completed

--- session1/index_start.js
+++ session1/index_start.js
@@ -59,8 +59,19 @@ function maxCharacter(str) {
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
-
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuz");
+    } else if (i % 3 === 0) {
+      console.log("Buzz");
+    } else if (i % 5 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 // Call Function
 const output = maxCharacter("javascript");
 
