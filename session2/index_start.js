6451131071bc62e865d429cd96ca25d3a1c6aa35
Mateen Kazia <mateenkazia@Mateens-MacBook-Pro.local>
2020-05-07 18:34:55 -0500

    anagram is working

--- session2/index_start.js
+++ session2/index_start.js
@@ -4,7 +4,7 @@
 function longestWord(sen) {
   const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
   const sorted = wordArr.sort((a, b) => b.length - a.length);
-  const filterArr = sorted.filter(word => word.length === sorted.length);
+  const filterArr = sorted.filter((word) => word.length === sorted.length);
   return filterArr.length === 1 ? filterArr[0] : filterArr;
 }
 
@@ -14,10 +14,8 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 function chunkArray(arr, len) {
   const chunkedArr = [];
-
-  arr.forEach(each => {
+  arr.forEach((each) => {
     const last = chunkedArr[chunkedArr.length - 1];
-
     return !last || last.length === len
       ? chunkedArr.push([each])
       : last.push(each);
@@ -37,7 +35,21 @@ function flattenArray(arrays) {
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 // Anagrams are word with the same length of charaters and can be rearranged to make new word
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let lookup = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    lookup[str1[i]] ? lookup[str1[i]]++ : (lookup[str1[i]] = 1);
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (!lookup[str2[i]]) return false;
+    lookup[str2[i]]--;
+  }
+
+  return true;
+}
 
 // CHALLENGE 5: Find Number
 // Find if the element is present in the array or not.
@@ -45,7 +57,7 @@ function isAnagram(str1, str2) {}
 // findNumber([1,2,3,4,5], 1) === YES
 // findNumber([1,2,3,4,5], 7) === NO
 function findNumber(arr, k) {
-  return arr.some(word => word === k) ? "YES" : "NO";
+  return arr.some((word) => word === k) ? "YES" : "NO";
 }
 
 // CHALLENGE 6: Odd Numbers
@@ -59,10 +71,10 @@ function oddNumbers(l, r) {
     arr.push(l);
   }
 
-  return arr.filter(n => n % 2);
+  return arr.filter((n) => n % 2);
 }
 
 // Call Function
-const output = oddNumbers(3, 9);
+const output = isAnagram("anagram", "nagaram");
 
 console.log(output);
