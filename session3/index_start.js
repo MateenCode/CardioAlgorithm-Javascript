64fac823bd97f971ffe61c8b8bcbb3e947190967
Mateen Kazia <mateenkazia@Mateens-MacBook-Pro.local>
2020-05-09 17:12:18 -0500

    finishing up

--- session3/index_start.js
+++ session3/index_start.js
@@ -35,7 +35,9 @@ function sumAllPrimes(num) {
 // Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-function seekAndDestroy(arr, ...rest) {}
+function seekAndDestroy(arr, ...rest) {
+  return arr.filter((val) => !rest.includes(val));
+}
 
 // CHALLENGE 4: SORT BY HEIGHT
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
@@ -43,7 +45,18 @@ function seekAndDestroy(arr, ...rest) {}
 // a = [-1, 150, 190, 170, -1, -1, 160, 180]
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 
-function sortByHeight(a) {}
+function sortByHeight(a) {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)));
+
+  const sortArr = arr2.sort((a, b) => a - b);
+
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+
+  return sortArr;
+}
 
 // CHALLENGE 5: MISSING LETTERS
 // Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
@@ -70,6 +83,8 @@ function evenOddSums(arr) {
   return [even, odd];
 }
 
-const output = sumAllPrimes(10);
+const a = [-1, 150, 160, 170, -1, -1, 180, 190];
+
+const output = sortByHeight(a);
 
 console.log(output);
